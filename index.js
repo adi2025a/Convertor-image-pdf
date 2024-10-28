314663a172e21e0cf03faee150c8f3cc64d82fc2
adi2025a <aadityasinghas13@gmail.com>
2024-10-28 17:59:37 +0530

    home.ejs complete

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const PORT =3000;
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //MIDDLEWARES
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,9 +18,17 @@ app.use(express.static(path.join(__dirname, 'uploads')));
 
 //ROUTES
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.render(path.join(__dirname,'views/home.ejs'));
 });
 
+app.get('/imagetopdf',(req,res)=>{
+    res.render(path.join(__dirname,'views/imagesToPdf.ejs'));
+})
+
+
+app.get('/pdftoimage',(req,res)=>{
+    res.render(path.join(__dirname,'views/pdfToImages.ejs'));
+})
 //PORT
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
